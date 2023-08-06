101e5057c41aa0cd3dcf2ac2b685c9d9c3d39089
LeNia0312 <141470703+LeNia0312@users.noreply.github.com>
2023-08-07 00:17:19 +0900

    add comment

--- Lazer/Assets/Script/BeamPhase/Asteroid/BeamPhaseAsteroidController.cs
+++ Lazer/Assets/Script/BeamPhase/Asteroid/BeamPhaseAsteroidController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 
 public class BeamPhaseAsteroidController : MonoBehaviour
 {
-
+    /// <summary>爆風エフェクトコントローラ</summary>
     [SerializeField]
     BoomController boomController;
+    
     public void Init()
     {
         this.gameObject.SetActive(true);
     }
-
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 自分を破壊
         Destroy(this.gameObject);
+
+        // 爆風を生成する
         var obj = Instantiate(boomController, this.transform.position, Quaternion.identity);
         obj.Init();
 
+        // 破壊カウントを加算して保持させる
         GameData.breakCount++;
     }
 }
