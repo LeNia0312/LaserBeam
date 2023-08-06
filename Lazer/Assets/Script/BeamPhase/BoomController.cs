2eb2120d2cdee01f496820790f7d10136c5610f6
LeNia0312 <141470703+LeNia0312@users.noreply.github.com>
2023-08-07 01:22:42 +0900

    add comment

--- Lazer/Assets/Script/BeamPhase/BoomController.cs
+++ Lazer/Assets/Script/BeamPhase/BoomController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class BoomController : MonoBehaviour
 {
-
+    // 爆風雨のアニメーター
     [SerializeField]
     private Animator anim;
 
+    /// <summary>
+    /// 初期化
+    /// </summary>
     public void Init()
-    {
+    {    
         this.gameObject.SetActive(true);
+
+        // 爆破アニメーション"boom_Play"を再生
         anim.Play("boom_Play", 0, 0);
     }
 }
