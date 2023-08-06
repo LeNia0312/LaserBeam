1f4c0a2b966336b7cc92353c1e457f88280d3159
LeNia0312 <141470703+LeNia0312@users.noreply.github.com>
2023-08-07 00:14:01 +0900

    add comment

--- Lazer/Assets/Script/GameData.cs
+++ Lazer/Assets/Script/GameData.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>ゲームのデータ保持クラス </summary>
 public static class GameData
 {
+    /// <summary>破壊モードでの破壊個数カウント</summary>
     public static int breakCount = 0;
 }
