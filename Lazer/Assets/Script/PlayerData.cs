436176deddd9ad3841d01e9b6eb61d198fd0db37
LeNia0312 <141470703+LeNia0312@users.noreply.github.com>
2023-08-07 00:14:40 +0900

    add comment

--- Lazer/Assets/Script/PlayerData.cs
+++ Lazer/Assets/Script/PlayerData.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>プレイヤーのデータ保持クラス</summary>
 public static class PlayerData
 {
-    public static float energy;
-        
+    /// <summary>チャージフェーズでチャージしたエネルギー保持</summary>
+    public static float energy;    
 }
