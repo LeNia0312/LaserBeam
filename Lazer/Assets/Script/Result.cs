11cc854257e772e8bb5e8af2a6d8c1033a292020
LeNia0312 <141470703+LeNia0312@users.noreply.github.com>
2023-08-07 00:15:30 +0900

    add comment

--- Lazer/Assets/Script/Result.cs
+++ Lazer/Assets/Script/Result.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 
 public class Result : MonoBehaviour
 {
+    /// <summary>破壊カウント表示テキスト</summary>
     [SerializeField]
     private Text breakCount;
 
     private void Start()
     {
+        // UI表示
         breakCount.text = GameData.breakCount.ToString();
     }
 
     void Update()
     {
+        // スペースキーでタイトルに戻る
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("TItle");
